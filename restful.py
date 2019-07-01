08f15ee11173b299d55c5b137a5070e268e593df
v-pogrebenko <52377901+v-pogrebenko@users.noreply.github.com>
2019-07-01 17:13:42 +0300

    Added headers check to test class
    - Updated test class to support response headers check.
    - Updated data files interactions.
    - Fixed empty headers json field error in response.

--- restful.py
+++ restful.py
@@ -5,23 +5,23 @@ import requests
 
 
 class Test:
-    """RESTful test class.
+    """REST API test class.
 
     Usage example:
 
     restful.Test(
-        "results_dir",
-        "sample_dir",
-        "tests_dir",
+        "responses_dir",
+        "samples_dir",
+        "requests_dir",
         "errors_dir",
     ).run()"""
 
-    def __init__(self, results_dir, sample_dir, tests_dir, errors_dir, encoding="utf-8"):
+    def __init__(self, responses_dir, samples_dir, requests_dir, errors_dir, encoding="utf-8"):
         """Class initialization.
 
-        :param results_dir: directory for restful responses.
-        :param sample_dir: directory for samples.
-        :param tests_dir: directory for restful requests.
+        :param responses_dir: directory for restful responses.
+        :param samples_dir: directory for samples.
+        :param requests_dir: directory for restful requests.
         :param errors_dir: directory for error output file.
         :param encoding: file open encoding."""
 
@@ -31,11 +31,11 @@ class Test:
             if not os.path.exists(directory):
                 os.makedirs(directory)
 
-        force_dir(results_dir)
-        self._results = results_dir
-        force_dir(sample_dir)
-        self._samples = sample_dir
-        self._tests = tests_dir
+        force_dir(responses_dir)
+        self._responses = responses_dir
+        force_dir(samples_dir)
+        self._samples = samples_dir
+        self._requests = requests_dir
         force_dir(errors_dir)
         self._errors = errors_dir
         self._encoding = encoding
@@ -46,18 +46,35 @@ class Test:
         :param name: destination file name."""
 
         # complete file name.
-        fn = os.path.join(self._tests, name + ".content")
+        fn = os.path.join(self._requests, name + ".cnt")
         # GET requests usually don't have content part.
         if not os.path.exists(fn):
             return ""
         # return contents.
+        return self._read_from_file(fn)
+
+    def _save_to_file(self, fn, data, mode="w"):
+        """Writes given data to a file.
+
+        :param fn: destination file name.
+        :param data: data to write.
+        :param mode: write mode."""
+
+        with open(fn, mode, encoding=self._encoding) as r:
+            r.write(data)
+
+    def _read_from_file(self, fn):
+        """Reads data from a file.
+
+        :param fn: source file name."""
+
         with open(fn, encoding=self._encoding) as f:
             return f.read()
 
-    def _send_request(self, ty, uri, headers, content, name):
+    def _send_request(self, method, uri, headers, content, name):
         """Forms and sends request to given handler and write response to a file
 
-        :param ty: request type.
+        :param method: request method.
         :param uri: request URI.
         :param headers: request headers.
         :param content: request content.
@@ -65,46 +82,83 @@ class Test:
 
         # establishes connection, forms and prepares a request then sends it.
         res = requests.Session().send(requests.Request(
-            ty, uri, headers=headers, data=content).prepare())
+            method, uri, headers=headers, data=content).prepare())
         res_content = res.content.decode(self._encoding)
-        # save result to a file.
-        with open(os.path.join(self._results, name + ".result"), "w", encoding=self._encoding) as r:
-            r.write(res_content)
+        # save contents and headers to a file.
+        self._save_to_file(
+            os.path.join(self._responses, name + ".cnt"),
+            res_content,
+        )
+        self._save_to_file(
+            os.path.join(self._responses, name + ".snh"),
+            json.dumps(
+                {
+                    "Status": res.status_code,
+                    "Headers": dict(res.headers),
+                },
+                indent=4,
+                sort_keys=True,
+            ),
+        )
 
-        return res_content
+        return res.status_code, res.headers, res_content
 
-    def _check_sample(self, name, res):
+    def _check_sample_contents(self, name, res):
         """Compares response contents with a sample.
 
         :param name: destination file name.
         :param res: response contents."""
 
         # complete file name.
-        sn = os.path.join(self._samples, name + ".sample")
-        rn = os.path.join(self._results, name + ".result")
+        sn = os.path.join(self._samples, name + ".cnt")
+        rn = os.path.join(self._responses, name + ".cnt")
         # if sample not exists (i.e. first run), make it.
         if not os.path.exists(sn):
-            with open(sn, "w", encoding=self._encoding) as s:
-                s.write(res)
+            self._save_to_file(sn, res)
 
-            return
-        # compare result to sample.
-        with open(sn, encoding=self._encoding) as s:
-            with open(rn, encoding=self._encoding) as r:
-                # write an error if contents are not equal.
-                if s.read() != r.read():
-                    self._write_err(name)
+            return True
+        # compare result to sample, write an error if contents are not equal.
+        return self._read_from_file(sn) == self._read_from_file(rn)
+
+    def _check_sample_headers(self, name, status, headers):
+        """Compares response headers and status code with a sample.
+
+        :param name: destination sample file name.
+        :param status: response status.
+        :param headers: response headers."""
+
+        # complete file name.
+        sn = os.path.join(self._samples, name + ".snh")
+        # if sample headers file does not exist, skip the check.
+        if not os.path.exists(sn):
+            return True
+        # read request parameters.
+        j = json.loads(self._read_from_file(sn))
+        sample_status = j.get("Status")
+        sample_headers = j.get("Headers")
+
+        if sample_status is not None and sample_status != status:
+            return False
+
+        if sample_headers is None:
+            return True
+
+        for header, value in sample_headers.items():
+            if headers.get(header) != value:
+                return False
+
+        return True
 
     def _get_names(self):
-        """Iterates through test files and get parameter names
+        """Iterates through request part files and get parameter names
 
-        :rtype: list of filenames"""
+        :rtype: list of file names"""
 
         # list files in directory and sort them by name.
-        files = os.listdir(self._tests)
+        files = os.listdir(self._requests)
         files.sort()
         # iterate through files and select parameter names.
-        return [file.rstrip(".params") for file in files if ".params" in file]
+        return [file.rstrip(".prm") for file in files if ".prm" in file]
 
     def _parse_param(self, fn):
         """Compares response contents with a sample.
@@ -113,35 +167,45 @@ class Test:
         :rtype: tuple of request type, URI and headers"""
 
         # read request parameters.
-        with open(os.path.join(self._tests, fn + ".params"), encoding=self._encoding) as f:
-            j = json.loads(f.read())
+        j = json.loads(self._read_from_file(
+            os.path.join(self._requests, fn + ".prm")))
 
-        return j["Type"], j["URI"], j["Headers"]
+        return j["Method"], j["URI"], j.get("Headers")
 
     def run(self):
         """Run tests, get results, compare them to samples and check for errors."""
 
         # at the start of a new test remove old errors.
         self._remove_errors()
-        # iterate through filenames, get corresponding data, do requests and compare results.
+        # iterate through file names, get corresponding data, do requests and compare results.
         for name in self._get_names():
-            ty, uri, headers = self._parse_param(name)
+            method, uri, headers = self._parse_param(name)
             req_content = self._get_request_content(name)
-            res_content = self._send_request(
-                ty, uri, headers, req_content, name)
-            self._check_sample(name, res_content)
+            res_status, res_headers, res_content = self._send_request(
+                method, uri, headers, req_content, name)
 
-    def _write_err(self, fn):
+            if not self._check_sample_headers(name, res_status, res_headers):
+                self._write_err(name)
+
+                continue
+
+            if not self._check_sample_contents(name, res_content):
+                self._write_err(name)
+
+                continue
+
+            print("Test #{} - OK".format(name))
+
+    def _write_err(self, name):
         """Writes given error to errors.txt
 
-        :param fn: name of a file that contains an error."""
+        :param name: name of a test that contains an error."""
 
         # print to std out.
-        msg = "ERROR during test # {} \n".format(fn)
-        print(msg)
+        msg = "Test #{} - FAIL\n".format(name)
+        print(msg, end="")
         # append to file.
-        with open(os.path.join(self._errors, "errors.txt"), "a", encoding=self._encoding) as f:
-            f.write(msg)
+        self._save_to_file(os.path.join(self._errors, "errors.txt"), msg, "a")
 
     def _remove_errors(self):
         """Removes errors.txt if exist."""
--- restful_example.py
+++ restful_example.py
@@ -6,8 +6,8 @@ import restful
 fd = os.path.dirname(__file__)
 # run tests (test #2 should fail).
 restful.Test(
-    os.path.join(fd, "data_example", "results"),
+    os.path.join(fd, "data_example", "responses"),
     os.path.join(fd, "data_example", "samples"),
-    os.path.join(fd, "data_example", "tests"),
+    os.path.join(fd, "data_example", "requests"),
     os.path.join(fd, "data_example", "errors"),
 ).run()
